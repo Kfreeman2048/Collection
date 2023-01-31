bd2a33840eed7e8cba59b0c0c3ccb65ee6280f9d
Kfreeman2048 <kfreeman2048@gmail.com>
2023-01-31 17:41:10 -0600

    add and test responsive columns

--- test.spec.ts
+++ test.spec.ts
@@ -12,3 +12,40 @@ test('changes theme', async ({ page }) => {
 
   await expect(button).toContainText('Dark');
 });
+
+test('render responsive design for tablet', async ({ page }) => {
+  await page.setViewportSize ({
+    width:  820,
+    height: 1180,
+  });
+  await page.goto('file:///C:/Users/kitha/code/website/index.html');
+
+  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+    return window.getComputedStyle(e).getPropertyValue("column-count")
+  });
+
+await expect(columnNumber).toBe("2");
+
+});
+
+test('render responsive design for mobile', async ({ page }) => {
+  await page.setViewportSize ({
+    width:  360,
+    height: 740,
+  });
+  await page.goto('file:///C:/Users/kitha/code/website/index.html');
+  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+      return window.getComputedStyle(e).getPropertyValue("column-count")
+  });
+
+  await expect(columnNumber).toBe("1");
+});
+
+test.only('displays input from form', async ({ page }) => {
+  await page.goto('file:///C:/Users/kitha/code/website/index.html');
+
+  await page.locator(`input[name = "textInput"]`).fill('wafflecones');
+  await page.locator(`button[name = "submitButton"]`).click();
+
+  await expect(await page.locator(`output[id = "x"]`)).toHaveText('wafflecones');
+});
