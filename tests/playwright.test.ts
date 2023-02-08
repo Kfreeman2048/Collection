17af3b6bd5f8c406b01a982d98148739f947ed63
Kfreeman2048 <kfreeman2048@gmail.com>
2023-02-07 19:02:20 -0600

    committing new repo setup

---
+++ tests/playwright.test.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+
+test('changes theme', async ({ page }) => {
+  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+  const button = page.locator(`button[id = 'themeChange']`);
+
+  await button.click();
+
+  await expect(button).toContainText('Light');
+
+  await button.click();
+
+  await expect(button).toContainText('Dark');
+});
+
+test('render responsive design for tablet', async ({ page }) => {
+  await page.setViewportSize ({
+    width:  820,
+    height: 1180,
+  });
+  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+
+  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+    return window.getComputedStyle(e).getPropertyValue("column-count")
+  });
+
+  await expect(columnNumber).toBe("2");
+
+});
+
+test('render responsive design for mobile', async ({ page }) => {
+  await page.setViewportSize ({
+    width:  360,
+    height: 740,
+  });
+  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+      return window.getComputedStyle(e).getPropertyValue("column-count")
+  });
+
+  await expect(columnNumber).toBe("1");
+});
+
+test('displays input from form', async ({ page }) => {
+  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+
+  await page.locator(`input[name = "textInput"]`).fill('wafflecones');
+  await page.locator(`button[name = "submitButton"]`).click();
+
+  await expect(await page.locator(`output[id = "x"]`)).toHaveText('wafflecones');
+});
