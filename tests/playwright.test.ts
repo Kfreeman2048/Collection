c221602bf1b969212edb388d6bb1c6afd2135842
Kfreeman2048 <kfreeman2048@gmail.com>
2023-02-15 16:41:52 -0600

    created groups for tests

--- tests/playwright.test.ts
+++ tests/playwright.test.ts
@@ -1,56 +1,62 @@
 import { test, expect } from '@playwright/test';
 
-test('changes theme', async ({ page }) => {
-  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
-  const button = page.locator(`button[id = 'themeChange']`);
+test.describe('window size tests', () => {
+  test('render responsive design for tablet', async ({ page }) => {
+    await page.setViewportSize ({
+      width:  820,
+      height: 1180,
+    });
+    await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
 
-  await button.click();
-
-  await expect(button).toContainText('Light');
-
-  await button.click();
-
-  await expect(button).toContainText('Dark');
-});
-
-test('render responsive design for tablet', async ({ page }) => {
-  await page.setViewportSize ({
-    width:  820,
-    height: 1180,
-  });
-  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
-
-  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
-    return window.getComputedStyle(e).getPropertyValue("column-count")
-  });
-
-  await expect(columnNumber).toBe("2");
-
-});
-
-test('render responsive design for mobile', async ({ page }) => {
-  await page.setViewportSize ({
-    width:  360,
-    height: 740,
-  });
-  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
-  const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+    const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
       return window.getComputedStyle(e).getPropertyValue("column-count")
+    });
+
+    await expect(columnNumber).toBe("2");
+
   });
 
-  await expect(columnNumber).toBe("1");
+  test('render responsive design for mobile', async ({ page }) => {
+    await page.setViewportSize ({
+      width:  360,
+      height: 740,
+    });
+    await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+    const columnNumber = await page.locator(`div[class = columnList]`).evaluate((e) => {
+        return window.getComputedStyle(e).getPropertyValue("column-count")
+    });
+
+    await expect(columnNumber).toBe("1");
+  });
+
 });
 
-test('returns if string has vowels', async ({ page }) => {
-  await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+test.describe('page functionality tests', () => {
+  test('changes theme', async ({ page }) => {
+    await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+    const button = page.locator(`button[id = 'themeChange']`);
 
-  await page.locator(`input[name = "textInput"]`).fill('wafflecones');
-  await page.keyboard.press('Enter');
+    await button.click();
 
-  await expect(await page.locator(`output[id = "x"]`)).toContainText('true');
+    await expect(button).toContainText('Light');
 
-  await page.locator(`input[name = "textInput"]`).fill('hccdf');
-  await page.keyboard.press('Enter');
+    await button.click();
 
-  await expect(await page.locator(`output[id = "x"]`)).toContainText('false');
+    await expect(button).toContainText('Dark');
+  });
+
+  test('returns if string has vowels', async ({ page }) => {
+    await page.goto('file:///C:/Users/kitha/code/Practice-Website/index.html');
+
+    await page.locator(`input[name = "textInput"]`).fill('wafflecones');
+    await page.keyboard.press('Enter');
+
+    await expect(await page.locator(`output[id = "x"]`)).toContainText('true');
+
+    await page.locator(`input[name = "textInput"]`).fill('hccdf');
+    await page.keyboard.press('Enter');
+
+    await expect(await page.locator(`output[id = "x"]`)).toContainText('false');
+  });
+
 });
